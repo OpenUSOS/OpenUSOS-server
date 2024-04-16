2bda73cdda654e61f2dcd3d5223b1416ae265aec
oskarkulinski <oskarkulinski3@gmail.com>
2024-04-16 16:42:33 +0200

    Added NotImplemented errors to usosapi.py

--- src/usosapi.py
+++ src/usosapi.py
@@ -1,37 +1,38 @@
 class USOSAPIConnection():
     pass
     def __init__(self):
-        pass
+        raise NotImplementedError
     #-----------------------------------------------------------------------------------
     def _generate_request_token(self):
-        pass
+        raise NotImplementedError
 
-    def is_anonymous(self) -> bool:
-        pass
+    def is_anonymous(self):
+        raise NotImplementedError
 
-    def is_authorized(self) -> bool:
-        pass
+    def is_authorized(self):
+        raise NotImplementedError
 
-    def test_connection(self) -> bool:
-        pass
+    def test_connection(self):
+        raise NotImplementedError
 
-    def get_authorization_url(self) -> str:
-        pass
+    def get_authorization_url(self):
+        raise NotImplementedError
 
-    def authorize_with_pin(self, pin: str):
-       pass
+    def authorize_with_pin(self):
+       raise NotImplementedError
 
-    def get_access_data(self) -> tuple:
-        pass
+    def get_access_data(self):
+        raise NotImplementedError
 
-    def set_access_data(self) -> bool:
-       pass
+    def set_access_data(self):
+       raise NotImplementedError
+
 
     def get(self):
-        pass
+        raise NotImplementedError
 
     def logout(self):
-        pass
+        raise NotImplementedError
 
     def current_identity(self):
-        pass
+        raise NotImplementedError
