be2c40e9cc24047273e4622b435534523a6142ce
filiera <filip.ciebiera@gmail.com>
2024-04-16 16:04:50 +0200

    klasy i ich funkcje
    dodano klasy i ich funkcje

--- setup.py
+++ setup.py
@@ -1,5 +1,35 @@
 import flet as ft
 
+class Schedule:
+    data = ''
+    def display(self):
+        pass
+
+class Emails:
+    data = ''
+    def display(self):
+        pass
+    def display_send(self):
+        pass
+    def send_email(self):
+        pass
+
+class Grades:
+    def display(self):
+        pass
+
+class Exams:
+    def display(self):
+        pass
+    def display_send(self):
+        pass
+
+class Reminders:
+    def set_reminders(self):
+        pass
+    def unset_reminders(self):
+        pass
+
 
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
